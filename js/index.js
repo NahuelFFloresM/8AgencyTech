5d59db0e0be0ea054066ac089b4027b4b316d552
nahuelflores <arcangel6191@gmail.com>
2021-01-12 17:37:21 -0300

    Agregado pantalla gris para modal

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById('submit_webinar_form').addEventListener('submit', function (event){
         event.preventDefault();
-        document.getElementById('screen_grey').classList.toggle('hidden');
+        document.getElementById('screen_grey').classList.toggle('fullscreen');
     });
 });
